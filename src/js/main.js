be6e628450cd23d1170bff2addb1df715a77154e
Luisk <ldelvalleu@ucenfotec.ac.cr>
2020-11-08 02:30:12 -0600

    Header and working request to api
    Header css and html (desktop only).
    Request to API, saving in local storage.
    Added images.
    Added colors.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
+const getToApi =  (pEmail) =>{
+	const url = `https://ltv-data-api.herokuapp.com/api/v1/records.json?email=${pEmail}`;
+	const resp =  fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		if( data != []){
+			localStorage.setItem('searchResult', JSON.stringify(data));
+		}
+	})
+}
 
+
+document.querySelector('#btnSubmitSearch').addEventListener('click', (e)=>{
+	e.preventDefault();
+	let email = document.querySelector('#txtEmailAddress').value;
+	getToApi(email);
+});
